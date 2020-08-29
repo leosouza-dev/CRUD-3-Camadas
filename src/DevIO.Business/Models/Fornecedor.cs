fce2fa84c29792fbad3e44db452ca89c379a6d17
sarival <sarival@sarival.com.br>
2020-08-29 11:37:27 -0300

    Add project files.

---
+++ src/DevIO.Business/Models/Fornecedor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace AppMvcBasica.Models
+{
+    public class Fornecedor : Entity
+    {
+        public string Nome { get; set; }
+        public string Documento { get; set; }
+        public TipoFornecedor TipoFornecedor { get; set; }
+        public bool Ativo { get; set; }
+        /* EF Relations */
+        public Endereco Endereco { get; set; } // fornecedor tem 1 endereço
+        public IEnumerable<Produto> Produtos { get; set; } // fornecedor tem varios produtos
+
+    }
+}
