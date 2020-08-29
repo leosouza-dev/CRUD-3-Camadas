d107e7fb9444d62fe0b246d3712103b47a37be9b
sarival <sarival@sarival.com.br>
2020-08-29 13:01:41 -0300

    Mapeando entidades com FluentAPI

--- src/DevIO.Data/Context/MeuDbContext.cs
+++ src/DevIO.Data/Context/MeuDbContext.cs
@@ -8,13 +8,18 @@ namespace DevIO.Data.Context
 {
     public class MeuDbContext : DbContext
     {
-        public MeuDbContext(DbContextOptions options) : base(options)
-        {
-
-        }
+        public MeuDbContext(DbContextOptions options) : base(options){}
 
         public DbSet<Produto> Produtos { get; set; }
         public DbSet<Endereco> Enderecos { get; set; }
         public DbSet<Fornecedor> Fornecedores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // configurando o mapeamento com FluentAPI (busca classe que implementem IEntityTypeConfiguration)
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuDbContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ src/DevIO.Data/Mappings/FornecedorMapping.cs
@@ -0,0 +1,32 @@
+﻿using AppMvcBasica.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DevIO.Data.Mappings
+{
+    public class FornecedorMapping : IEntityTypeConfiguration<Fornecedor>
+    {
+        public void Configure(EntityTypeBuilder<Fornecedor> builder)
+        {
+            builder.HasKey(f => f.Id);
+
+            builder.Property(f => f.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            builder.Property(f => f.Documento)
+                .IsRequired()
+                .HasColumnType("varchar(14)");
+
+            builder.ToTable("Fornecedores");
+
+            // relacionamento
+            builder.HasOne(f => f.Endereco)
+                .WithOne(e => e.Fornecedor); // 1:1
+
+            builder.HasMany(f => f.Produtos) // n:1
+                .WithOne(p => p.Fornecedor)
+                .HasForeignKey(p => p.FornecedorId);
+        }
+    }
+}
