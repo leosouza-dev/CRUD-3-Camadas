b1a68782b71b40ae8158fc83fbaf5809cfc7c885
sarival <sarival@sarival.com.br>
2020-08-31 19:14:46 -0300

    Registrando Dependencias na classe startup

--- src/DevIO.App/Startup.cs
+++ src/DevIO.App/Startup.cs
@@ -14,6 +14,8 @@ using DevIO.App.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using DevIO.Data.Context;
+using DevIO.Business.Interfaces;
+using DevIO.Data.Repository;
 
 namespace DevIO.App
 {
@@ -48,6 +50,12 @@ namespace DevIO.App
             services.AddDefaultIdentity<IdentityUser>()
                 .AddDefaultUI(UIFramework.Bootstrap4)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            // REGISTRANDO AS DEPENDENCIAS
+            services.AddScoped<MeuDbContext>();
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+            services.AddScoped<IEnderecoRepository, EnderecoRepository>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
