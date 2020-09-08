0422a85f78708f4d71da7c9b3b8bd89d78c90b53
sarival <sarival@sarival.com.br>
2020-09-08 17:59:14 -0300

    criando provider - precisamos criar um provider para usar o Adapter

--- src/DevIO.App/Extensions/MoedaAttribute.cs
+++ src/DevIO.App/Extensions/MoedaAttribute.cs
@@ -51,4 +51,20 @@ namespace DevIO.App.Extensions
             return "Moeda em formato inválido";
         }
     }
+
+    // precisamos criar um provider para usar o Adapter
+    public class MoedaValidationAttributeAdapterProvider : IValidationAttributeAdapterProvider
+    {
+        private readonly IValidationAttributeAdapterProvider _baseProvider = new ValidationAttributeAdapterProvider();
+
+        public IAttributeAdapter GetAttributeAdapter(ValidationAttribute attribute, IStringLocalizer stringLocalizer)
+        {
+            if (attribute is MoedaAttribute moedaAttribute)
+            {
+                return new MoedaAttributeAdapter(moedaAttribute, stringLocalizer);
+            }
+
+            return _baseProvider.GetAttributeAdapter(attribute, stringLocalizer);
+        }
+    }
 }
